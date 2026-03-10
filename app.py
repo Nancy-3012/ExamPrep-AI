9520ea910db16e87151477713c939c123ac8bdd0
Palak Majethia <palakmajethia@gmail.com>
2026-03-10 23:50:43 +0530

    Add question count selector

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ st.title("ExamPrep AI")
 st.write("Upload your syllabus or notes and generate exam questions.")
 
 uploaded_file = st.file_uploader("Upload syllabus or notes")
+
+num_questions = st.selectbox(
+    "Select number of questions",
+    [5,10,15]
+)
